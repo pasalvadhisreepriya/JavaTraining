49ab473fc160e0fd81d2efcf1b12dd0d3645bec5
pasalvadhisreepriya <pasalvadhi.sreepriya@kellton.com>
2023-06-26 20:54:31 +0530

    Initial commit

---
+++ src/CodingExercise/Main.java
@@ -0,0 +1,28 @@
+package CodingExercise;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the length of the floor = ");
+        double length = scanner.nextDouble();
+
+        System.out.print("Enter the width of the floor = ");
+        double width = scanner.nextDouble();
+
+        System.out.print("Enter the cost per square meter of carpet = ");
+        double costPerSquareMeter = scanner.nextDouble();
+
+        scanner.close();
+
+        // Create objects
+        Floor floor = new Floor(width, length);
+        Carpet carpet = new Carpet(costPerSquareMeter);
+        Calculator calculator = new Calculator(floor, carpet);
+
+        // Calculate and display total cost
+        double totalCost = calculator.getTotalCost();
+        System.out.println("Total cost = " + totalCost);
+    }
+}
